058bd8dae18acb816cc32811c585c0b938d40c5b
Tomás Salina <salinatomass53@gmail.com>
2021-11-24 15:04:38 -0300

    Creating a server with Express.js

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
-const http = require("http");
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
-});
+const express = require("express");
+const app = express();
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Express server</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const note = notes.find((item) => item.id === Number(id));
+
+  note
+    ? res.json(note)
+    : res.status(404).json({ message: "Invalid id", error: "404 not found" });
+});
+
+app.post("/api/notes", (req, res) => {
+  const { content, important } = req.body;
+
+  if (!content) res.status(400).json({ error: "content is missing" });
+
+  const newNote = {
+    id: notes.length + 1,
+    content,
+    important: important || false,
+    date: new Date().toISOString(),
+  };
+
+  notes = [...notes, newNote];
+
+  res.status(201).json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  notes = notes.filter((note) => note.id !== Number(id));
+  res.status(204).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
