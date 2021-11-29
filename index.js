03d9f081d83166672f528f5d8c070d335fc17027
Tomás Salina <salinatomass53@gmail.com>
2021-11-29 20:45:50 -0300

    Notes API

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./loggerMiddleware')
 
-const PORT = 3001;
+const PORT = 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
     important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
     important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
     important: true,
   },
-];
+]
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('<h1>Express server</h1>')
+})
 
-app.get("/", (req, res) => {
-  res.send("<h1>Express server</h1>");
-});
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
-app.get("/api/notes/:id", (req, res) => {
-  const { id } = req.params;
-  const note = notes.find((item) => item.id === Number(id));
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  const note = notes.find(item => item.id === Number(id))
 
   note
     ? res.json(note)
-    : res.status(404).json({ message: "Invalid id", error: "404 not found" });
-});
+    : res.status(404).json({ message: 'Invalid id', error: '404 not found' })
+})
 
-app.post("/api/notes", (req, res) => {
-  const { content, important } = req.body;
+app.post('/api/notes', (req, res) => {
+  const { content, important } = req.body
 
-  if (!content) res.status(400).json({ error: "content is missing" });
+  if (!content) res.status(400).json({ error: 'content is missing' })
 
   const newNote = {
     id: notes.length + 1,
     content,
     important: important || false,
     date: new Date().toISOString(),
-  };
+  }
 
-  notes = [...notes, newNote];
+  notes = [...notes, newNote]
 
-  res.status(201).json(newNote);
-});
+  res.status(201).json(newNote)
+})
 
-app.delete("/api/notes/:id", (req, res) => {
-  const { id } = req.params;
-  notes = notes.filter((note) => note.id !== Number(id));
-  res.status(204).end();
-});
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  notes = notes.filter(note => note.id !== Number(id))
+  res.status(204).end()
+})
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not found',
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
