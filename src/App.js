c3cb3a0bf756e5519a65d1ea29a0b1ffe76cd574
Tomás Salina <salinatomass53@gmail.com>
2021-11-29 21:21:16 -0300

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from "react";
-import Notes from "./components/Notes";
+import React, { useState, useEffect } from 'react';
+import Notes from './components/Notes';
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+const API_URL = 'http://localhost:3001';
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setNotes(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <h1>Notes</h1>
